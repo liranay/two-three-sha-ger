ed5b35c4fdf6442a09de6eb72e1bc615d02f0474
ronyjacobson <ronyjacobson@gmail.com>
2023-11-16 17:08:50 +0200

    Adding filters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu';
+import FiltersMenu from './components/FiltersMenu';
 import Tags from './components/FilterAndSorter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ContentList from './components/ContentList';
@@ -12,6 +13,7 @@ function App() {
         <Menu/>
         <Tags/>
         <ContentList/>
+        <FiltersMenu/>
     </div>
   );
 }
