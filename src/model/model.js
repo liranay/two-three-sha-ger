0822d701d1ee5df63859cd16addea1a391fb3007
liran <liranai@gmail.com>
2023-11-16 18:13:03 +0200

    pictures

--- src/model/model.js
+++ src/model/model.js
@@ -21,31 +21,107 @@ export const LANGUAGE = {
 };
 
 export const posts = [
-    {
-        filename: "1.jpeg",
-        title: "This is the first post",
-        description: "This is the first post",
-        tags: ["1", "2"],
-        date: Date.now(),
-        language: LANGUAGE.ENGLISH,
-        rating: 4,
-      },
-      {
-        filename: "2.jpeg",
-        title: "This is the first post",
-        description: "This is the first post",
-        tags: ["1", "2"],
-        date: Date.now(),
-        language: LANGUAGE.ENGLISH,
-        rating: 4,
-      },
-      {
-        filename: "3.jpeg",
-        title: "This is the first post",
-        description: "This is the first post",
-        tags: ["1", "2"],
-        date: Date.now(),
-        language: LANGUAGE.ENGLISH,
-        rating: 4,
-      },
+  {
+    filename: "1.jpeg",
+    title: "Who is the villain?",
+    description: "This is the first post",
+    tags: ["shieldren"],
+  },
+  {
+    filename: "2.jpeg",
+    title: "Same Jihadist terror, different years.",
+    description: "This is the first post",
+    tags: ["tError", "HamasisISIS", "TheWestIsNext"],
+  },
+  {
+    filename: "3.jpeg",
+    title: "True shared values and who aims to disrupt them..",
+    description:
+      "People tear down the American flag,In pro-Israeli protests they raise the American flag high.",
+    tags: ["Westurn", "Woke", "ProPalestineRally", "StandWithIsrael"],
+  },
+  {
+    filename: "4.jpeg",
+    title: "This is not human. Right(s)?.",
+    // description: "This is the first post",
+    tags: ["Shieldren"],
+  },
+  {
+    filename: "5.jpeg",
+    title:
+      "After all hostages—men, women, and children—are immediately released and safely return to Israel.",
+    description: `
+    *After Hamas is defeated and no longer has the capability to commit massacres or fire rockets into Israel, and after its leaders surrender to Israel.
+    *After securing Gaza residents with an international solution that is not aligned with Hamas and ensures their human rights.`,
+    tags: ["CeasefireNOW"],
+  },
+  {
+    filename: "6.jpeg",
+    title: "Is this your strategic shielding?",
+    // description: "This is the first post",
+    tags: ["Shieldren"],
+  },
+  {
+    filename: "7.jpeg",
+    title: "How to avoid #Propagaza in 7 steps. Credit: Senia Waldberg",
+    description: `1. There is no such thing as hundreds of dead in one bombing. That's why the story of the hospital in the first place sounds crazy. In order for hundreds of people to be killed in one bombing, a very large amount of very powerful bombs must be dropped and an entire block of buildings must be wiped out. This will not necessarily bring you to this situation either. In Russia, the record in the entire war was just over 100 dead in one Ukrainian bombing of a building. And even then, they did not exceed the 130 dead. So any news in which Hamas claims that several hundred people have died is fake news. Unless you see a hole in the ground the size of a football field, and even then it's probably a fake. ‎ ‎ ‎ ‎
+
+    2. Don't believe any number published by Hamas! Not because they don't know how to count, but because in the eyes of Hamas, human life has no meaning and they like to inflate numbers to make themselves poor.
+    
+    3. When you see lots of bodies arranged in bags, it's a fake. In Gaza, nothing works properly and certainly not quickly. So if you see an order in the dead or bodies in bags, it's a fake. It's staged on purpose. The goal is to photograph it of course and show the world.
+    
+    4. Pay attention to the differences between the news: Externally Hamas may be whining, but internally Hamas tries to present that everything is under control and its fighters are brave (bravely hiding in the tunnels). That's why they constantly spread news about their successes, which they know that no one will really check that they are false.
+    
+    5. Attempts to sway people: A good lie is built gradually. First they spread some news that something big is going to happen, and after a few hours or a day, they gloat that it happened and succeeded of course. The Russians do it all the time. After a few days when the truth becomes clear they cool down their enthusiasm of course, but in Gaza no one will check. This is how they try to "sweep the crowd" or generate some kind of momentum. Yes, it's stupid because in the end the truth becomes clear. But the Russians also do it all the time and in the span of a few days it certainly succeeds in raising morale. Then we will probably see a lot of news like "tonight our forces are planning a surprise for the enemy", then a failed attempt of some Hamas operation in which 20 terrorists will be killed, and Hamas will regret that the operation was successful.
+    
+    6. They know that their public does not read Hebrew and will not check the truth. They barely read Arabic. But a picture of those 20 terrorists being eliminated, on the other hand, will damage their image. Especially if the photo is in the exact same place where Hamas claimed success, then it will be difficult for them to continue with the engraving. For people who have the mentality of a 6-year-old child, you have to talk in pictures.
+    
+    7. #HamasisISIS card is the 243 civillans hostages that were kidnapped on OCT 7`,
+    tags: ["Propagaza"],
+  },
+  {
+    filename: "8.jpeg",
+    title:
+      "Israel Defense Forces spokesperson: operational headquarters, weapons and technological equipment in the #MRI building ‼️at #ShifaHospital",
+    description: ``,
+    tags: ["ShifaTerrorHQ", "WarCrimes"],
+  },
+  {
+    filename: "9.jpeg",
+    title:
+      "What does the Red Cross do for human rights?",
+    description: `Yesterday?
+    Today?
+    
+    I expect that a global organization that aims to protect human rights, will do everything to bring the precious innocents home to the arms of their families.
+    
+    where are you?
+    
+    #hamasisisis terrorists have been holding our #240hostages for #41days, including over #30children, the elderly, women, and men.
+    
+    We want our families back. #bringthemhomenow
+    
+    We are facing a religious and cultural war against Western culture.
+    
+    We must unite against this radical hate. Western Culture will prevail. 🌎`,
+    tags: ["BringThemHomeNow" , "Hostages"],
+  },
+  {
+    filename: "10.jpeg",
+    title: "Am Israel Chai",
+    description: ``,
+    tags: ["BringThemHomeNow" , "Hostages"],
+  },
+  {
+    filename: "11.jpeg",
+    title: "No, Women, No cry.",
+    description: `This is YOU! if you dont WAKE UP!
+    Don’t sell who you are to a religious death cult, #HamasisISIS
+    
+    There are 240 Israeli Cevilans and other nationalities that are hostages in Gaza, 30 of them are kids. #bringthemhomenow #bringthemhome
+    
+    Credit: Yana Yanuchka`,
+    tags: ["TheWestIsNext"],
+  },
+  
 ];
