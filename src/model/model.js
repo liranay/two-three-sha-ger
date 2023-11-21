4bbf69876c66a8d0c512456509186d18a6ca8bd3
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-11-21 19:57:15 +0200

    Update model.js

--- src/model/model.js
+++ src/model/model.js
@@ -28,6 +28,8 @@ export const posts = [
       "TheWestIsNext",
       "CeasefireNOW",
       "Propagaza",
+       "ellakenan",
+      
     ],
   },
   {
@@ -51,7 +53,7 @@ export const posts = [
   {
     filename: "5.jpg",
     title:
-      "After all hostages—men, women, and children—are immediately released and safely return to Israel.",
+      "*After all hostages—men, women, and children—are immediately released and safely return to Israel.",
     description: "",
     tags: [
       "Hostages",
@@ -59,6 +61,7 @@ export const posts = [
       "CeasefireNOW",
       "StandWithIsrael",
       "shieldren",
+      "ellakenan",
     ],
   },
   {
@@ -69,18 +72,19 @@ export const posts = [
   {
     filename: "7.jpg",
     title: "How to avoid #Propagaza? Credit: @Kerenaz",
-    tags: ["Propagaza", "Woke", "MediaBias", "TheWestIsNext", "CeasefireNOW"],
+    tags: ["Propagaza", "Woke", "MediaBias", "TheWestIsNext", "CeasefireNOW", "KerenAzaria"],
   },
   {
     filename: "8.jpg",
     title:
-      "Israel Defense Forces spokesperson: operational headquarters, weapons and technological equipment in the #MRI building at #ShifaHospital",
+      "Operational headquarters, weapons and technological equipment in the #MRI building at #ShifaHospital",
     tags: [
       "ShifaTerrorHQ",
       "WarCrimes",
       "HamasisISIS",
       "HumanRights",
       "TheWestIsNext",
+      "IDF spokesperson"
     ],
   },
   {
@@ -113,10 +117,8 @@ export const posts = [
     description: `This is YOU! if you don't WAKE UP!
   Don’t sell who you are to a religious death cult, #HamasisISIS
  
-  There are 240 Israeli Civilians and other nationalities that are hostages in Gaza, 30 of them are kids. #bringthemhomenow #bringthemhome
- 
-  Credit: Yana Yanuchka`,
-    tags: ["TheWestIsNext", "Hostages", "BringThemHomeNow", "WarCrimes"],
+  There are 240 Israeli Civilians and other nationalities that are hostages in Gaza, 30 of them are kids. #bringthemhomenow #bringthemhome`,
+    tags: ["TheWestIsNext", "Hostages", "BringThemHomeNow", "WarCrimes", " Yana Yanuchka"],
   },
   {
     filename: "12.jpg",
@@ -194,71 +196,222 @@ export const posts = [
   {
     filename: "19.jpg",
     title: "Choose your club",
-    tags: ["HamasisISIS", "WarCrimes", "HumanRights"],
+    tags: ["HamasisISIS", "WarCrimes", "HumanRights", "EllaKenan"],
   },
-  {
-    filename: "20.jpg",
-    title: "Food for thought.",
-    tags: [
-      "Shieldren",
-      "HamasisISIS",
-      "WarCrimes",
-      "HumanRights",
-      "CeasefireNOW",
-    ],
-  },
+ 
   {
     filename: "22.png",
     title: "Is it Illegal to Defend Myself?",
-    tags: ["FAKR", "FakeNews"],
+    tags: [ "HamasisISIS",
+      "WarCrimes",
+      "HumanRights",
+      "CeasefireNOW"],
   },
 
   {
     filename: "23.png",
     title: "Does the Quran allow this atrocity?",
-    tags: ["Nazi", "NeoNazi", "SHAME", "Koran", "Islam", "Religion"],
+    tags: ["Islam", "Religion", "HamasisISIS", "WarCrimes",],
   },
 
   {
     filename: "24.png",
     title: "FreePalestine = FreeVandalists",
-    tags: ["CRIME", "Vandalists"],
+    tags: [ "Vandalists", "HamasisISIS", "WarCrimes",],
   },
 
   {
     filename: "25.png",
     title: "May Hell be with You",
-    tags: ["BloodShoes", "WarCrime"],
+    tags: [ "WarCrimes", "Hostages"],
   },
 
   {
     filename: "26.jpeg",
     title: "Where are you UN Woman?",
-    tags: ["WomanRights", "RIGHTS"],
+    tags: ["WomanRights", "Hostages"],
   },
 
   {
     filename: "27.png",
     title: "#MeJew",
-    tags: ["Jews", "Holocaust"],
+    tags: ["Jews", "Holocaust", "GazaLight", "MeJewMeToo"],
   },
 
   {
     filename: "28.png",
     title: "You Know Nothing",
-    tags: ["HAMAS", "Foolish"],
+    tags: ["HAMAS", "Woke", "Propagaza", "HamasisISIS", "TheWestIsNext", "HumanRights"],
   },
 
   {
     filename: "29.png",
     title: "Your Child for Dinner? Why Not?",
-    tags: ["BURN", "CHILD", "ALIVE"],
+    tags: ["WarCrimes", "Jews", "Holocaust"],
   },
 
   {
     filename: "30.png",
     title: "FREE PALESTINE?",
-    tags: ["USELESS", "NoWhatToFree"],
+    tags: ["UsefullIdiots", "Woke", "Propagaza", "HamasisISIS", "TheWestIsNext", "HumanRights"],
+  },
+  {
+    filename: "31.png",
+    title: "Is Hamas Human?",
+    tags: ["HamasisISIS", "FreeGazaFromHamas"],
+  },
+
+
+{
+    filename: "32.png",
+    title: "BRING THEM HOME !",
+    tags: ["RedCross", "StandWithUs"],
+  },
+
+
+{
+    filename: "33.png",
+    title: "Shame on You Harvard",
+    tags: ["University", "standwithisrael","StopAntisemitism"],
+  },
+
+
+{
+    filename: "34.png",
+    title: "Nevr Again?",
+    tags: ["HamasisISIS", "bringthemhome", "standwithisrael"],
+  },
+
+{
+    filename: "35.png",
+    title: "They’re All the SAME",
+    tags: ["HamasisISIS", "Nazism"],
+  },
+
+
+{
+    filename: "36.png",
+    title: "BRING THEM HOME, NOW !!!",
+    tags: ["bringthemhome", "standwithisrael"],
+  },
+
+
+{
+    filename: "37.png",
+    title: "Why is There a Time Limit Only When it Comes to Israel?",
+    tags: ["standwithisrael", "bringthemhome"],
+  },
+
+
+{
+    filename: "38.png",
+    title: "I’M OK ?",
+    tags: ["HamasisISIS", "InnocentWomen", "InnocentChildren"],
+  },
+
+
+{
+    filename: "39.png",
+    title: "Welcom to the Puppets Show",
+    tags: ["Hypocrisy", "HamasisISIS", "FakeNews"],
+  },
+
+
+{
+    filename: "40.png",
+    title: "Oops..",
+    tags: ["HamasisISIS","Hypocrisy"]
+  },
+
+
+{
+    filename: "41.png",
+    title: "Who is the Giving Tree?",
+    tags: ["HumanitarianCrisisLie", "HamasisISIS","Hypocrisy"],
+  },
+
+{
+    filename: "42.png",
+    title: "World’s Twisted Point of View",
+    tags: ["HamasisISIS", , "standwithisrael"],
+  },
+  
+{
+    filename: "100.jpg",
+    title: "UN secretary general, UNICEF and United Nations do not care about Israeli children. #WorldChildrensDay #standwithisrael Credit:@ellakenan",
+    tags: ["WorldChildrensDay", "standwithisrael","ellakenan" ],
+  },
+
+{
+    filename: "101.jpg",
+    title: "This children's day we call for the return of our children and all hostages. #WorldChildrensDay #standwithisrael #BringThemBack #BringThemBackHome #Hamasisisis Credit:@ellakenan",
+    tags: ["BringThemBack", "BringThemBackHome", "Hamasisisis" ,"WorldChildrensDay", "standwithisrael","ellakenan" ],
+  },
+  
+{
+    filename: "102.jpg",
+    title: "Please take an immediate stance against Jihadi terrorist support on campus. #standwithisrael #Hamasisisis Credit:@ellakenan",
+    tags: ["Hamasisisis", "standwithisrael", "ellakenan" ],
+  },
+{
+    filename: "103.jpg",
+    title: "Gen Z in the US is really the biggest disappointment of all time. #standwithisrael #TheWestIsNext #Educateyourself Credit:@ellakenan",
+    tags: ["TheWestIsNext","Educateyourself","ellakenan" ],
+  },  
+{
+    filename: "104.jpg",
+    title: "The New American Idol? #TheWestIsNext #Educateyourself #standwithisrael Credit:@ellakenan",
+    tags: ["TheWestIsNext","Educateyourself","ellakenan" ],
+  }, 
+{
+    filename: "105.jpg",
+    title: "Did you ever question yourself why was it so easy to make you a useful idiot in the service of ISIS? #standwithisrael #HamasisISIS #Educateyourself Credit:@ellakenan",
+    tags: ["TheWestIsNext","Educateyourself", "UsefullIdiots" ,"ellakenan" ],
+  }, 
+{
+    filename: "106.jpg",
+    title: "Imagine that Hamas-ISIS has infiltrated your village, murdered you friends, family and kidnapped you days before giving birth. #standwithisrael #HamasisISIS #WomanRights Credit:@ellakenan",
+    tags: ["InnocentChildren", "bringthemhome", "WomanRights" ,"ellakenan" ],
+  }, 
+{
+    filename: "107.jpg",
+    title: "An Israeli woman, in the last weeks of her final trimester, was kidnapped to Gaza on October 7th and had to give birth while in captivity. #standwithisrael #HamasisISIS #WomanRights Credit:@ellakenan",
+    tags: ["InnocentChildren", "bringthemhome", "WomanRights" ,"ellakenan" ],
+  }, 
+{
+    filename: "108.jpg",
+    title: "Roger Waters, WE know what happened. Hamas terrorists broke into our cities, butchered our citizens, raped our women, and took our children hostage. #standwithisrael  #HamasisISIS #Educateyourself  Credit:@ellakenan",
+    tags: ["TheWestIsNext","Educateyourself", "UsefullIdiots" ,"ellakenan" ],
+  }, 
+{
+    filename: "109.jpg",
+    title: "Current state of higher education in the US. #standwithisrael #hamasisisis #educateyourself #education #learning #university Credit:@ellakenan",
+    tags: ["University","Educateyourself", "UsefullIdiots" ,"ellakenan" ],
+  },
+{
+    filename: "110.jpg",
+    title: "This is how you look. #standwithisrael #hamasisisis #educateyourself #education #learning #university Credit:@ellakenan",
+    tags: ["University","Educateyourself", "UsefullIdiots" ,"ellakenan" ],
+  },
+ {
+    filename: "111.jpg",
+    title: "Imagine failing to understand the undeniable facts - It's Israel now, your home is next.Angelina I'm talking to you. @Angelina Jolie #thewestisnext #standwithisrael #freegazafromhamas Credit:@ellakenan",
+    tags: ["standwithisrael","Educateyourself", "freegazafromhamas" ,"ellakenan" ],
+  },
+ {
+    filename: "112.jpg",
+    title: "Hamas is the sole cause of suffering both in Israel and in Gaza. #freepalestinefromhamas #humanrights #humanity #standwithisrael #freegazafromhamas Credit:@ellakenan",
+    tags: ["standwithisrael","Educateyourself", "humanrights", "freegazafromhamas" ,"ellakenan" ],
+  },
+{
+    filename: "113.jpg",
+    title: "Amongst the 241 people there are more than 30 babies and children, some without their parents! #bringthemhomenow #humanrights #humanity #standwithisrael #freegazafromhamas Credit:@ellakenan",
+    tags: ["InnocentChildren", "bringthemhome", "humanrights","ellakenan" ],
+  },
+  {
+    filename: "114.jpg",
+    title: "There cannot be a humanitarian ceasefire withou their release. 9 months-year-old babies! 85-year-olds! Please share.#HamasisISIS #TheWestlsNext #BringThemBack #BringThemHomeNow #standwithisrael Credit:@ellakenan",
+    tags: ["InnocentChildren", "bringthemhome", "humanrights","HamasisISIS","TheWestlsNext", "BringThemBack", "BringThemHomeNow", "standwithisrael", "ellakenan" ],
   },
 ];
 export const getTags = () => {
