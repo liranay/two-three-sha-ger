fe7290c116fa6660792977bc586606e2afc7ee64
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-11-21 15:46:09 +0200

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,67 +1,50 @@
-import React, { useState } from "react";
-import { Image } from "react-bootstrap";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import React from 'react';
 import ShareIcon from "@mui/icons-material/Share";
-import CommentIcon from "@mui/icons-material/Comment";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 function Post({ title, fileName, tags }) {
-  const handleOnClick = async () => {
-    if (!navigator.share) {
-      alert("Your browser does not support sharing");
-    }
-    if (navigator.share) {
-      const protocol = window.location.protocol;
-      const hostname = window.location.hostname;
-      const port = window.location.port;
-      const url = `${protocol}//${hostname}:${port}/photos/${fileName}`;
-      const rawContent = await fetch(url);
-      const blob = await rawContent.blob();
-      const data = {
-        files: [
-          new File([blob], fileName, {
-            type: "image/png",
-          }),
-        ],
-        title,
-        text: title,
-      };
+  const url = `${window.location.origin}/photos/${fileName}`;
+  const encodedTitle = encodeURIComponent(title);
+  const encodedUrl = encodeURIComponent(url);
 
-      if (navigator.canShare(data)) {
-        try {
-          await navigator.share(data);
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.error(err.name, err.message);
-          }
-        } finally {
-          return;
-        }
-      } else {
-        alert("can't share");
+  // URLs for sharing on different social media platforms
+  const socialMediaShareUrls = {
+    twitter: `https://twitter.com/intent/tweet?text=${encodedTitle}&url=${encodedUrl}`,
+    facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`,
+    whatsapp: `https://api.whatsapp.com/send?text=${encodedTitle} ${encodedUrl}`,
+    linkedIn: `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`,
+    telegram: `https://t.me/share/url?url=${encodedUrl}&text=${encodedTitle}`,
+    // Add more platforms if needed
+  };
+
+  const handleShareToAll = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: title,
+          text: title,
+          url: url,
+        });
+        console.log('Content shared successfully');
+      } catch (err) {
+        console.error('Error sharing:', err);
       }
+    } else {
+      // Fallback for browsers that do not support the Web Share API
+      Object.values(socialMediaShareUrls).forEach(shareUrl => {
+        window.open(shareUrl, '_blank');
+      });
     }
   };
 
   return (
-    <div
-      className="post"
-      style={{
-        borderRadius: "8px",
-        background: "var(--background-panel-day, #FFF)",
-        boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-      }}
-    >
-      <img className="post__image" src={`/photos/${fileName}`} alt="dsa" />
-      <h4 className="post__text">
-        <strong>{title}</strong>
-      </h4>
-      <h6>{tags.map((tag) => `#${tag} `)}</h6>
+    <div className="post" style={{ borderRadius: "8px", background: "var(--background-panel-day, #FFF)", boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}>
+      <img className="post__image" src={url} alt={title} />
+      <h4 className="post__text"><strong>{title}</strong></h4>
+      <h6>{tags.map(tag => `#${tag} `)}</h6>
 
       <div style={{ display: "flex", margin: "15px" }}>
-        {/* <FavoriteBorderIcon /> */}
-        <ShareIcon style={{ marginLeft: "5px" }} onClick={handleOnClick} />
-        {/* <CommentIcon style={{ marginLeft: "5px" }} /> */}
+        <ShareIcon style={{ marginLeft: "5px" }} onClick={handleShareToAll} />
         <BookmarkBorderIcon style={{ marginLeft: "5px" }} />
       </div>
     </div>
