927d376f37159b1c0b5183395ebea6119e02cad7
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-11-21 17:59:23 +0200

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,53 +1,55 @@
-import React, { useState } from "react";
+import React from "react"; // You don't need to import useState if you're not using it
 import { Image } from "react-bootstrap";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShareIcon from "@mui/icons-material/Share";
-import CommentIcon from "@mui/icons-material/Comment";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 function Post({ title, fileName, tags }) {
   const handleOnClick = async () => {
     if (!navigator.share) {
       alert("Your browser does not support sharing");
+      return; // Return early if sharing is not supported
     }
-    if (navigator.share) {
-      const protocol = window.location.protocol;
-      const hostname = window.location.hostname;
-      const port = window.location.port;
-      const url = `${protocol}//${hostname}:${port}/photos/${fileName}`;
+
+    const protocol = window.location.protocol;
+    const hostname = window.location.hostname;
+    const port = window.location.port;
+    const url = `${protocol}//${hostname}:${port}/photos/${fileName}`;
+
+    try {
       const rawContent = await fetch(url);
+
+      if (!rawContent.ok) {
+        throw new Error("Network response was not ok");
+      }
+
       const blob = await rawContent.blob();
       const data = {
-        files: [
-          new File([blob], fileName, {
-            type: "image/png",
-          }),
-        ],
+        files: [new File([blob], fileName, { type: "image/png" })],
         title,
         text: title,
       };
 
       if (navigator.canShare(data)) {
-        try {
-          await navigator.share(data);
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.error(err.name, err.message);
-          }
-        } finally {
-          return;
-        }
+        await navigator.share(data);
       } else {
-        alert("can't share");
+        alert("Can't share");
+      }
+    } catch (err) {
+      if (err.name !== "AbortError") {
+        console.error(err.name, err.message);
       }
     }
   };
+
   return (
     <div
       className="post"
-	@@ -52,16 +49,14 @@ function Post({ title, fileName, tags }) {
+      style={{
+        borderRadius: "8px",
+        background: "var(--background-panel-day, #FFF)",
         boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       }}
+    >
       <img className="post__image" src={`/photos/${fileName}`} alt="dsa" />
       <h4 className="post__text">
         <strong>{title}</strong>
@@ -55,9 +57,11 @@ function Post({ title, fileName, tags }) {
       <h6>{tags.map((tag) => `#${tag} `)}</h6>
 
       <div style={{ display: "flex", margin: "15px" }}>
-        {/* <FavoriteBorderIcon /> */}
         <ShareIcon style={{ marginLeft: "5px" }} onClick={handleOnClick} />
-        {/* <CommentIcon style={{ marginLeft: "5px" }} /> */}
         <BookmarkBorderIcon style={{ marginLeft: "5px" }} />
       </div>
     </div>
+  );
+}
+
+export default Post;
