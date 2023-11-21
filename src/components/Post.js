37e78a4cdfa21050c13cfc52d35b758a5fcc9b8e
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-11-21 14:12:19 +0200

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,58 +1,5 @@
 import React, { useState } from "react";
 import { Image } from "react-bootstrap";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import ShareIcon from "@mui/icons-material/Share";
-import CommentIcon from "@mui/icons-material/Comment";
-import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-
-function Post({ title, fileName, tags }) {
-  const handleOnClick = async () => {
-    if (!navigator.share) {
-      alert("Your browser does not support sharing");
-    }
-    if (navigator.share) {
-      const protocol = window.location.protocol;
-      const hostname = window.location.hostname;
-      const port = window.location.port;
-      const url = `${protocol}//${hostname}:${port}/photos/${fileName}`;
-      const rawContent = await fetch(url);
-      const blob = await rawContent.blob();
-      const data = {
-        files: [
-          new File([blob], fileName, {
-            type: "image/png",
-          }),
-        ],
-        title,
-        text: title,
-      };
-
-      if (navigator.canShare(data)) {
-        try {
-          await navigator.share(data);
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.error(err.name, err.message);
-          }
-        } finally {
-          return;
-        }
-      } else {
-        alert("can't share");
-      }
-    }
-  };
-
-  return (
-    <div
-      className="post"
-      style={{
-        borderRadius: "8px",
-        background: "var(--background-panel-day, #FFF)",
-        boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-      }}
-    >import React, { useState } from "react";
-import { Image } from "react-bootstrap";
 import ShareIcon from "@mui/icons-material/Share";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
@@ -121,20 +68,3 @@ function Post({ title, fileName, tags }) {
 
 export default Post;
 
-      <img className="post__image" src={`/photos/${fileName}`} alt="dsa" />
-      <h4 className="post__text">
-        <strong>{title}</strong>
-      </h4>
-      <h6>{tags.map((tag) => `#${tag} `)}</h6>
-
-      <div style={{ display: "flex", margin: "15px" }}>
-        {/* <FavoriteBorderIcon /> */}
-        <ShareIcon style={{ marginLeft: "5px" }} onClick={handleOnClick} />
-        {/* <CommentIcon style={{ marginLeft: "5px" }} /> */}
-        <BookmarkBorderIcon style={{ marginLeft: "5px" }} />
-      </div>
-    </div>
-  );
-}
-
-export default Post;
