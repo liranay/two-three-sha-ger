e23237197baf3a76fe337c8f82dcc2dfa5e0ab43
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-11-22 10:39:49 +0200

    Update ContentList.js

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,31 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Post from "./Post";
 import * as model from "../model/model";
 
-function ContentList() {
-  // Initialize state to store posts data
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    // Define an async function to fetch posts data from the API
-    async function fetchPosts() {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/hasbara/posts/");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        // Update the state with the fetched posts data
-        setPosts(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-
-    // Call the fetchPosts function to fetch data when the component mounts
-    fetchPosts();
-  }, []); // The empty dependency array ensures that this effect runs once when the component mounts
-
+function ContentList({ posts }) {
   return (
     <div className="timeline">
       {posts.map((post, index) => (
@@ -33,7 +10,7 @@ function ContentList() {
           key={index}
           title={post.title}
           description={post.description}
-          fileName={post.media}
+          fileName={post.filename}
           tags={post.tags}
         />
       ))}
@@ -42,4 +19,3 @@ function ContentList() {
 }
 
 export default ContentList;
-
